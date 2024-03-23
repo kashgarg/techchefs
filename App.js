f0a23df1467b69a2966700ca906957e796d392a7
kashgarg <kashishgarg247@gmail.com>
2024-03-23 12:47:12 -0700

    Initialized React Native with Expo

--- App.js
+++ App.js
@@ -1,22 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, } from 'react-native';
-import CameraButton from './components/CameraButton';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
+      <Text>Open up App.js to start working on your app!!!</Text>
       <StatusBar style="auto" />
-      <CameraButton />
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#08d',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
