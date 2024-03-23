dcd889cc7b3efccebdf28f7c5ed63724317da5bd
mayadong-korotu <maya.dong@korotu.com>
2024-03-23 13:57:12 -0700

    Landing page initial change

---
+++ components/LandingPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const LandingPage = () => {
+  return (
+    <View style={styles.container}>
+      {/* Your content goes here */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FECBFF', // Pink background
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default LandingPage;
